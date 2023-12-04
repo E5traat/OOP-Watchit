eef1b31bfba790ebdfcd5b7b9e4ee550f298a9e7
elrefai <114250600+Abo-hmeed@users.noreply.github.com>
2023-12-05 00:51:10 +0200

    Update User.java

--- User.java
+++ User.java
@@ -81,6 +81,7 @@ public class User {
     }
 
     public void watchMovie(int movieIndex){
+        MovieList.arr.get(movieIndex).increaseViews();
         historyMovies.add(MovieList.arr.get(movieIndex));
         System.out.println("movie is watched successfully!");
     }
