2f4e08eca2467b7dc28845c887d66f02ae7ac772
ElAlbany <128701669+ElAlbany@users.noreply.github.com>
2023-12-04 15:23:33 +0200

    Update UserWatchRecordList.java

--- UserWatchRecordList.java
+++ UserWatchRecordList.java
@@ -1,36 +1,35 @@
 import java.util.ArrayList;
 
 public class UserWatchRecordList {
-    static ArrayList<UserWatchRecord> watchRecords = new ArrayList<>();
+    static ArrayList<UserWatchRecord> watchRecords = new ArrayList<> ();
 
     public void addRecord(UserWatchRecord r) {
-        watchRecords.add(r);
+        watchRecords.add (r);
     }
 
-    public void removeRecords(int ID){
-        for (int i = 0; i < watchRecords.size(); i++) {
-            if(watchRecords.get(i).getUserID() == ID){
-                watchRecords.remove(watchRecords.get(i));
+    public void removeRecords(int ID) {
+        for (int i = 0; i < watchRecords.size (); i++) {
+            if (watchRecords.get (i).getUserID () == ID) {
+                watchRecords.remove (watchRecords.get (i));
             }
         }
     }
 
-    static public void updateRating(int ID,String movieName,float rate){
+    static public void updateRating(int ID, String movieName, float rate) {
         for (UserWatchRecord watchRecord : watchRecords) {
-            if (watchRecord.getUserID() == ID && watchRecord.getMovieName().equals(movieName)) {
-                watchRecord.setMovieRating(rate);
+            if (watchRecord.getUserID () == ID && watchRecord.getMovieName ().equals (movieName)) {
+                watchRecord.setMovieRating (rate);
             }
         }
     }
 
-    static public float returnRate(int ID,String movieName){
+    static public float returnRate(int ID, String movieName) {
         for (UserWatchRecord watchRecord : watchRecords) {
-            if (watchRecord.getUserID() == ID && watchRecord.getMovieName().equals(movieName)) {
+            if (watchRecord.getUserID () == ID && watchRecord.getMovieName ().equals (movieName)) {
                 return watchRecord.getMovieRating ();
             }
         }
 
-
-
-
+        return 0.0f;
     }
+}
