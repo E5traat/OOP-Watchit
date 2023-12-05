ab7716f2c348a2e80e6ff56822c7d510573677f3
ElAlbany <128701669+ElAlbany@users.noreply.github.com>
2023-12-05 20:18:02 +0200

    Update User.java

--- User.java
+++ User.java
@@ -2,6 +2,9 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class User {
+    Basic b = new Basic ();
+    Standard st = new Standard ();
+    Premium pr = new Premium ();
     public final int ID;
     public static int instance = 0;
     public String userName;
@@ -11,19 +14,19 @@ public class User {
     public String email;
     public String subscription;
     public int moviesWatched;
-    public ArrayList<Movie> laterMovies = new ArrayList<Movie>();
-    public ArrayList<Movie> historyMovies = new ArrayList<Movie>();
+    public ArrayList<Movie> laterMovies = new ArrayList<Movie> ();
+    public ArrayList<Movie> historyMovies = new ArrayList<Movie> ();
 
 
-    Scanner input = new Scanner(System.in);
+    Scanner input = new Scanner (System.in);
 
-    public User(){
+    public User() {
         instance++;
         ID = instance;
     }
 
-    public User(String userName, String firstName, String lastName, String password, String email){
-        this();
+    public User(String userName, String firstName, String lastName, String password, String email) {
+        this ();
         this.userName = userName;
         this.firstName = firstName;
         this.lastName = lastName;
@@ -31,20 +34,52 @@ public class User {
         this.email = email;
 
     }
+
+    public ArrayList<Movie> getLaterMovies() {
+        return laterMovies;
+    }
+
+    public void setLaterMovies(ArrayList<Movie> laterMovies) {
+        this.laterMovies = laterMovies;
+    }
+
+    public ArrayList<Movie> getHistoryMovies() {
+        return historyMovies;
+    }
+
+    public void setHistoryMovies(ArrayList<Movie> historyMovies) {
+        this.historyMovies = historyMovies;
+    }
+
+    public String getSubscription() {
+        return subscription;
+    }
+
+    public void setSubscription(String subscription) {
+        this.subscription = subscription;
+    }
+
     public int getID() {
         return ID;
     }
 
-    public String getUserName(){return userName;}
-    public void setUserName(String name){userName=name;}
-    public void showWatchHistory(){
-        for(Movie m : historyMovies){
-            System.out.println( (historyMovies.indexOf(m) + 1)  + ")" + m.getTitle());
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String name) {
+        userName = name;
+    }
+
+    public void showWatchHistory() {
+        for (Movie m : historyMovies) {
+            System.out.println ((historyMovies.indexOf (m) + 1) + ")" + m.getTitle ());
         }
     }
-    public void showWatchLater(){
-        for(Movie m : laterMovies){
-            System.out.println( (historyMovies.indexOf(m) + 1)  + ")" + m.getTitle());
+
+    public void showWatchLater() {
+        for (Movie m : laterMovies) {
+            System.out.println ((historyMovies.indexOf (m) + 1) + ")" + m.getTitle ());
         }
     }
 
@@ -80,14 +115,76 @@ public class User {
         this.email = email;
     }
 
-    public void watchMovie(int movieIndex){
-        MovieList.arr.get(movieIndex).increaseViews();
-        historyMovies.add(MovieList.arr.get(movieIndex));
-        System.out.println("movie is watched successfully!");
-    }
-    public void watchMovieLater(int movieIndex){
-        laterMovies.add(MovieList.arr.get(movieIndex));
-        System.out.println("movie is added to later movies successfully!");
+    public void watchMovie(int userIndex, int movieIndex) {
+        int userId1 = UserList.arr.get (userIndex).getID ();
+        if (UserList.arr.get (userIndex).getSubscription ().equals ("Basic")) {
+            if (b.subDays (userId1) == false) {
+                System.out.print ("you have passed 30 days");
+                System.out.print ("you need to subscribe again");
+                System.out.println ();
+            } else if (b.maxWatch (UserList.arr.get (userIndex).getHistoryMovies ()) == false) {
+                System.out.println ("sorry, " + UserList.arr.get (userIndex).getUserName ());
+                System.out.println ("you have reached your maximum watching movies");
+            } else {
+                MovieList.arr.get (movieIndex).increaseViews ();
+                historyMovies.add (MovieList.arr.get (movieIndex));
+                System.out.println ("\tWatched successfully");
+                UserWatchRecord uwr = new UserWatchRecord (userId1,
+                        MovieList.arr.get (movieIndex).getTitle ());
+                UserWatchRecordList.addRecord (uwr);
+            }
+        } else if (UserList.arr.get (userIndex).getSubscription ().equals ("Standard")) {
+            if (st.subDays (userId1) == false) {
+                System.out.print ("you have passed 30 days");
+                System.out.print ("you need to subscribe again");
+                System.out.println ();
+            } else if (st.maxWatch (UserList.arr.get (userIndex).getHistoryMovies ()) == false) {
+                System.out.println ("sorry, " + UserList.arr.get (userIndex).getUserName ());
+                System.out.println ("you have reached your maximum watching movies");
+            } else {
+                MovieList.arr.get (movieIndex).increaseViews ();
+                historyMovies.add (MovieList.arr.get (movieIndex));
+                System.out.println ("\tWatched successfully");
+                UserWatchRecord uwr = new UserWatchRecord (userId1,
+                        MovieList.arr.get (movieIndex).getTitle ());
+                UserWatchRecordList.addRecord (uwr);
+            }
+        } else if (UserList.arr.get (userIndex).getSubscription ().equals ("Premium")) {
+            if (pr.subDays (userId1) == false) {
+                System.out.print ("you have passed 30 days");
+                System.out.print ("you need to subscribe again");
+                System.out.println ();
+            } else if (pr.maxWatch (UserList.arr.get (userIndex).getHistoryMovies ()) == false) {
+                System.out.println ("sorry, " + UserList.arr.get (userIndex).getUserName ());
+                System.out.println ("you have reached your maximum watching movies");
+
+            } else {
+                MovieList.arr.get (movieIndex).increaseViews ();
+                historyMovies.add (MovieList.arr.get (movieIndex));
+                System.out.println ("\tWatched successfully");
+                UserWatchRecord uwr = new UserWatchRecord (userId1,
+                        MovieList.arr.get (movieIndex).getTitle ());
+                UserWatchRecordList.addRecord (uwr);
+            }
+        }
+
     }
 
+    public void watchMovieLater(int movieIndex) {
+        laterMovies.add (MovieList.arr.get (movieIndex));
+        System.out.println ("movie is added to later movies successfully!");
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "ID=" + ID +
+                ", userName='" + userName + '\'' +
+                ", password='" + password + '\'' +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", email='" + email + '\'' +
+                ", subscription='" + subscription + '\'' +
+                '}';
+    }
 }
