8f1a50eeef5ee3552406ead123872235ccf00dbc
ElAlbany <128701669+ElAlbany@users.noreply.github.com>
2023-12-05 20:15:35 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -9,13 +9,13 @@ public class Main {
 
         AdminList a1 = new AdminList ();
         MovieList m1 = new MovieList ();
-        CastList c1 = new CastList();
-        UserList u1 = new UserList();
+        CastList c1 = new CastList ();
+        UserList u1 = new UserList ();
         DirectorList d1 = new DirectorList ();
         System.out.println (a1.toString ());
 
         while (true) {
-            boolean adminExist=false;
+            boolean adminExist = false;
             int userIndex = -1;
 
             System.out.println ("\t\t welcome to  Watchit");
@@ -27,114 +27,93 @@ public class Main {
             if (choice == 3) //exit
                 break;
             else if (choice == 1) //log in
-            {   while(true) {
-
-                System.out.flush ();
-                System.out.println ("Enter your username");
-                String username = input.next ();
-                System.out.println ("Enter your password");
-                String password = input.next ();
-                adminExist = a1.adminExist (username, password);
-                userIndex = u1.userExist (username, password);
-
-                if (userIndex != -1 || adminExist == true) {
-                    break;
-                }
-                else {
-                    System.out.println ("\t\tinvalid username or password");
-                    System.out.println ("\t\tplease try again");
-                    continue;
-                }
-            }
-            }
-
-
-            else if(choice==2) //sign up user
             {
-                userIndex = u1.queryUser();
+                while (true) {
+
+                    System.out.flush ();
+                    System.out.println ("Enter your username");
+                    String username = input.next ();
+                    System.out.println ("Enter your password");
+                    String password = input.next ();
+                    adminExist = a1.adminExist (username, password);
+                    userIndex = u1.userExist (username, password);
+
+                    if (userIndex != -1 || adminExist == true) {
+                        break;
+                    } else {
+                        System.out.println ("\t\tinvalid username or password");
+                        System.out.println ("\t\tplease try again");
+                        continue;
+                    }
+                }
+            } else if (choice == 2) //sign up user
+            {
+                userIndex = u1.queryUser ();
             }
 
-            while(adminExist==true)
-            {
+            while (adminExist == true) {
                 a1.adminGeneralMenu ();
-                choice=input.nextInt ();
-                if(choice==5)
+                choice = input.nextInt ();
+                if (choice == 5)
                     break;
-                else if(choice==1)
-                {
-                    while(true) {
+                else if (choice == 1) {
+                    while (true) {
                         m1.displayAllMovies ();
                         m1.movieMenu ();
                         choice = input.nextInt ();
                         if (choice == 1) {
                             m1.newMovie ();
                             continue;
-                        }
-                        else if(choice==2) {
+                        } else if (choice == 2) {
                             System.out.println ("please select one movies to update");
                             int index = input.nextInt ();
                             m1.updateMoviesMenu (index);
                             continue;
-                        }
-                        else if(choice==3)
-                        {
+                        } else if (choice == 3) {
                             System.out.println ("please select one movie to remove");
-                            int index=input.nextInt ();
+                            int index = input.nextInt ();
                             m1.deleteMovie (index);
                             continue;
-                        }
-                        else if(choice==4)
-                        {
+                        } else if (choice == 4) {
                             System.out.println ("enter the movie you want to see its details");
-                            int index =input.nextInt ();
+                            int index = input.nextInt ();
                             m1.showMovieDetails (index);
-                        }
-                        else if(choice==5)
-                        {
+                        } else if (choice == 5) {
                             break;
 
                         }
                     }
 
-                }
-                else if(choice == 2){
+                } else if (choice == 2) {
 
-                    while(true) {
-                        c1.displayAllActors();
-                        c1.castMenu();
+                    while (true) {
+                        c1.displayAllActors ();
+                        c1.castMenu ();
                         choice = input.nextInt ();
                         if (choice == 1) {
                             c1.newCast ();
                             continue;
-                        }
-                        else if(choice==2) {
+                        } else if (choice == 2) {
                             System.out.println ("please select one cast to update");
                             int index = input.nextInt ();
                             c1.updateCastsMenu (index);
                             continue;
-                        }
-                        else if(choice==3)
-                        {
+                        } else if (choice == 3) {
                             System.out.println ("please select one actor to remove");
-                            int index=input.nextInt ();
-                            c1.deleteCast (index -1);
+                            int index = input.nextInt ();
+                            c1.deleteCast (index - 1);
                             continue;
-                        }
-                        else if(choice==4)
-                        {
+                        } else if (choice == 4) {
                             System.out.println ("enter the cast you want to see its details");
-                            int index =input.nextInt ();
-                            c1.showActorDetails(index);
-                        }
-                        else if(choice==5)
-                        {
+                            int index = input.nextInt ();
+                            c1.showActorDetails (index);
+                        } else if (choice == 5) {
                             break;
 
                         }
                     }
 
-                }
-                else if (choice == 3) {
+                } else if (choice == 3) {
 
                     while (true) {
                         d1.displayAllDirectors ();
@@ -179,21 +158,17 @@ public class Main {
                             System.out.println ("\t\tselect an admin to update");
                             int num = input.nextInt ();
                             a1.updateAdminMenu (num);
-                        }
-                        else if (choice == 6) {
+                        } else if (choice == 6) {
                             a1.diplayAdmins ();
                             System.out.println ("\t\tselect an admin to delete");
                             int num = input.nextInt ();
                             a1.deleteAdmin (num);
-                        }
-                        else if (choice == 7) {
+                        } else if (choice == 7) {
                             a1.diplayAdmins ();
                             System.out.println ("\t\tselect an admin to see his/her details");
                             int num = input.nextInt ();
-                            a1.arrAdmin.get (num-1).adminDetails ();
-                        }
-
-                        else if (choice == 8) {
+                            a1.arrAdmin.get (num - 1).adminDetails ();
+                        } else if (choice == 8) {
                             break;
                         } else {
                             System.out.println ("\t\tinvalid operation");
@@ -205,59 +180,76 @@ public class Main {
 
 
             }
-            while (userIndex != -1){
+            while (userIndex != -1) {
 
-                u1.userMenu();
+                u1.userMenu ();
 
-                choice=input.nextInt ();
-                if(choice==9)
-                    break;
-                else if(choice==1)  // watch movie
+                choice = input.nextInt ();
+
+                if (choice == 1)  // watch movie
                 {
-                    MovieList.displayAllMovies();
-                    System.out.println("Enter the number movie you want to watch:");
-                    int movieChoice = input.nextInt();
-                    UserList.arr.get(userIndex).watchMovie(movieChoice - 1);
-                }
-                else if(choice == 2){  // watch movie later
+                    MovieList.displayAllMovies ();
+                    System.out.println ("Enter the number movie you want to watch:");
+                    int movieChoice = input.nextInt ();
+                    UserList.arr.get (userIndex).watchMovie (userIndex, movieChoice - 1);
+                } else if (choice == 2) {  // watch movie later
 
-                    MovieList.displayAllMovies();
-                    System.out.println("Enter the number movie you want to watch later:");
-                    int movieChoice = input.nextInt();
-                    UserList.arr.get(userIndex).watchMovieLater(movieChoice - 1);
+                    MovieList.displayAllMovies ();
+                    System.out.println ("Enter the number movie you want to watch later:");
+                    int movieChoice = input.nextInt ();
+                    UserList.arr.get (userIndex).watchMovieLater (movieChoice - 1);
 
-                }
-                else if(choice == 3){       // show History
+                } else if (choice == 3) {       // show History
 
-                    u1.arr.get(userIndex).showWatchHistory();
+                    u1.arr.get (userIndex).showWatchHistory ();
 
-                }
-                else if(choice == 4){       // show watch later
+                } else if (choice == 4) {       // show watch later
 
-                    u1.arr.get(userIndex).showWatchLater();
+                    u1.arr.get (userIndex).showWatchLater ();
 
-                }
-                else if(choice == 5){       // see movie details
+                } else if (choice == 5) {       // see movie details
 
-                    MovieList.displayAllMovies();
-                    System.out.println("Enter the movie you want to see it's details:");
-                    int movieChoice = input.nextInt();
-                    MovieList.arr.get(movieChoice -1).displayMovieDetails();
-                }
-                else if(choice == 6){       // search
-                    u1.recursiveSearch();
-                }
-                else if(choice == 7){       // most viewed movies
-                    u1.mostWatchedMovies();
-                }
-                else if(choice == 8){       // delete user
-                    u1.deleteUser(userIndex);
+                    MovieList.displayAllMovies ();
+                    System.out.println ("Enter the movie you want to see it's details:");
+                    int movieChoice = input.nextInt ();
+                    MovieList.arr.get (movieChoice - 1).displayMovieDetails ();
+
+                } else if (choice == 6) {       // search
+                    u1.recursiveSearch ();
+
+                } else if (choice == 7) {       // most viewed movies
+                   m1.displayTopWatchedMovies ();
+
+                } else if (choice == 8) {       // delete user
+                    u1.deleteUser (userIndex);
                     break;
+
+                } else if (choice == 9) {       // update user
+                    u1.updateUserMenu (userIndex);
+
+                } else if (choice == 10) {       // add rate
+                    u1.addRate (userIndex);
+
+                } else if (choice == 11) {       // Resubscribe
+                    Subscription.subscribeAgain (userIndex);
+
+                } else if (choice == 12) {       // Recent movies
+                    m1.displayRecentMovies ();
+
+                } else if (choice == 13) {       // Top rated movies
+                    m1.displayTopRatedMovies ();
+
+                }
+                else if (choice == 14) {       // Show profile
+                    System.out.println (UserList.arr.get (userIndex).toString ());
+                    System.out.println ();
+
+                }else if (choice == 15) { //break;
+                    break;
+
                 }
 
             }
-
-
 
 
         }
