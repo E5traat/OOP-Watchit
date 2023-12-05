3a7626d9e86f35a8c4725e89b3979204b25015c2
ElAlbany <128701669+ElAlbany@users.noreply.github.com>
2023-12-05 20:18:52 +0200

    Update UserWatchRecordList.java

--- UserWatchRecordList.java
+++ UserWatchRecordList.java
@@ -3,11 +3,11 @@ import java.util.ArrayList;
 public class UserWatchRecordList {
     static ArrayList<UserWatchRecord> watchRecords = new ArrayList<> ();
 
-    public void addRecord(UserWatchRecord r) {
+    public static void addRecord(UserWatchRecord r) {
         watchRecords.add (r);
     }
 
-    public void removeRecords(int ID) {
+    public static void removeRecords(int ID) {
         for (int i = 0; i < watchRecords.size (); i++) {
             if (watchRecords.get (i).getUserID () == ID) {
                 watchRecords.remove (watchRecords.get (i));
@@ -15,19 +15,19 @@ public class UserWatchRecordList {
         }
     }
 
-    static public float updateRating(int ID, String movieName, float rate) {
+     public static float updateRating(int ID, String movieName, float rate) {
         for (UserWatchRecord watchRecord : watchRecords) {
             if (watchRecord.getUserID () == ID && watchRecord.getMovieName ().equals (movieName)) {
                 watchRecord.setMovieRating (rate);
                 break;
             }
         }
-           return movieRate (movieName);
+        return movieRate (movieName);
     }
 
     public static float movieRate(String title) {
         float sumRates = 0.0f;
-        float counter = 0;
+        int counter = 0;
         for (UserWatchRecord watchRecord : watchRecords) {
             if (watchRecord.getMovieName ().equals (title)) {
                 if (watchRecord.getMovieRating () != 0.0f) {
