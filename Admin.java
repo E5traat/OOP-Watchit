dc29eeace78763327b55eda545982cb4aad9d4fd
E5traat <154227409+E5traat@users.noreply.github.com>
2023-12-21 00:18:58 +0200

    Update Admin.java

--- Admin.java
+++ Admin.java
@@ -4,13 +4,24 @@ import java.util.Scanner;
 public class Admin implements Serializable {
     static Scanner input = new Scanner (System.in);
     public static int number_of_admins = 0;
-    private final int id;
+    private int id;
     private String username;
     private String password;
     private String firstName;
     private String lastName;
     private String email;
 
+    public static int getNumber_of_admins() {
+        return number_of_admins;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
 
     public String getUsername() {
         return username;
@@ -28,15 +39,29 @@ public class Admin implements Serializable {
         this.password = password;
     }
 
+    public String getFirstName() {
+        return firstName;
+    }
+
     public void setFirstName(String firstName) {
         this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
     }
 
     public void setLastName(String lastName) {
         this.lastName = lastName;
     }
 
+    public String getEmail() {
+        return email;
+    }
 
+    public void setEmail(String email) {
+        this.email = email;
+    }
 
     public Admin(String username, String password, String firstName, String lastName, String email) {
         this.username = username;
@@ -71,4 +96,5 @@ public class Admin implements Serializable {
         }
     }
     }
+
 }
